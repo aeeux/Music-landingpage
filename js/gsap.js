3a67be4f68797792224b2120687881c662d7514b
aeeux <AndreasEriksen99@hotmail.com>
2021-04-26 19:00:18 +0200

    update
    update

---
+++ js/gsap.js
@@ -0,0 +1,112 @@
+
+
+TweenMax.from(".left-col", 2, {
+      width: "0%",
+      ease: Expo.easeInOut
+})
+
+TweenMax.from(".logo", 2, {
+      delay: 2.5,
+      y: 20,
+      opacity: 0,
+      ease: Expo.easeInOut
+})
+
+TweenMax.from(".buy", 2, {
+      delay: 2.8,
+      y: 20,
+      opacity: 0,
+      ease: Expo.easeInOut
+})
+
+TweenMax.from(".year", 2, {
+      delay: 3.1,
+      y: 20,
+      opacity: 0,
+      ease: Expo.easeInOut
+})
+
+TweenMax.from(".album-name", 2, {
+      delay: 2.4,
+      y: 20,
+      opacity: 0,
+      ease: Expo.easeInOut
+})
+
+TweenMax.from(".album-artist", 2, {
+      delay: 2.6,
+      y: 20,
+      opacity: 0,
+      ease: Expo.easeInOut
+})
+
+TweenMax.from(".about h1", 2, {
+      delay: 3,
+      y: 20,
+      opacity: 0,
+      ease: Expo.easeInOut
+})
+
+TweenMax.from(".album-name2", 2, {
+      delay: 3.2,
+      y: 20,
+      opacity: 0,
+      ease: Expo.easeInOut
+})
+
+TweenMax.from(".album-artist2", 2, {
+      delay: 3.4,
+      y: 20,
+      opacity: 0,
+      ease: Expo.easeInOut
+})
+
+TweenMax.from(".album-genre2", 2, {
+      delay: 3.6,
+      y: 20,
+      opacity: 0,
+      ease: Expo.easeInOut
+})
+
+TweenMax.from(".media p", 2, {
+      delay: 3.6,
+      y: 20,
+      opacity: 0,
+      ease: Expo.easeInOut
+})
+
+TweenMax.staggerFrom(".media ul li", 2, {
+      delay: 3.8,
+      opacity: 0,
+      y: 20,
+      ease: Expo.easeInOut
+}, 0.1);
+
+
+TweenMax.from(".album-name3", 2, {
+      delay: 4,
+      y: 20,
+      opacity: 0,
+      ease: Expo.easeInOut
+})
+
+TweenMax.from(".album-artist3", 2, {
+      delay: 4.2,
+      y: 20,
+      opacity: 0,
+      ease: Expo.easeInOut
+})
+
+TweenMax.from(".next", 2, {
+      delay: 4.4,
+      y: 20,
+      opacity: 0,
+      ease: Expo.easeInOut
+})
+
+TweenMax.from(".more", 2, {
+      delay: 4.6,
+      y: 20,
+      opacity: 0,
+      ease: Expo.easeInOut
+})
